d2df2626bacf356f608c56465a127aa951dfebcf
Lucas Stein <stein.lucas@icloud.com>
2023-04-26 20:49:22 -0300

    change bd password

--- bd/Conexao.php
+++ bd/Conexao.php
@@ -2,7 +2,7 @@
 class Conexao {
 	protected $conn;
 	function __construct() {
-		$this->conn = new PDO ( "mysql:host=localhost;dbname=estante", "root", "123" );
+		$this->conn = new PDO ( "mysql:host=localhost;dbname=estante", "root", "" );
 	}
 	public function obterConexao() {
 		return $this->conn;
